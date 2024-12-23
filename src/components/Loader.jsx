43ee33a9330570fb53c7776210605398498eaf33
project-group123 <projectgroup468@gmail.com>
2024-12-23 21:24:06 +0100

    uodate

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,24 +8,25 @@ const Loader = () => {
         setloading(true)
         setTimeout(() => {
             navigate("/Login")
-        }, 6000)
+        },6000)
     })
     if (loading) {
         return (
-            <div className='body' >
-                <div className="loader">
-                    <div className="logo">
-                        <img src="https://res.cloudinary.com/dgxvuw8wd/image/upload/v1734938938/amend_lntakp.png" alt="" />
-                        <h1>Attendipen</h1>
-                    </div>
-                    <section class="dots-container">
-                        <div class="dot"></div>
-                        <div class="dot"></div>
-                        <div class="dot"></div>
-                        <div class="dot"></div>
-                        <div class="dot"></div>
-                    </section>
+            <div>
+                <div className="container">
+                    <div className="loader">
+                        <div className="logo">
+                            <img src="https://res.cloudinary.com/dgxvuw8wd/image/upload/v1734982730/attend_in8wmy.png" alt="" />
+                        </div>
+                        <section class="dots-container">
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                        </section>
 
+                    </div>
                 </div>
             </div>
         )
