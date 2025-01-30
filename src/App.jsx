341968cf28e3065bf0e23b2c7a757559fba8419a
damola ifatifo <ifatifod4@gmail.com>
2025-01-30 14:41:21 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 import Loader from './components/Loader'
 import Register from './components/Register'
+import Teachers from './components/Teachers'
+import Student from './components/Student'
+import Billings from './components/Billings'
+import Setting from './components/Setting'
+import Exams from './components/Exams'
 
 const App = () => {
   return (
@@ -13,6 +18,11 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/Dashboard" element={<Dashboard />} />
       <Route path="/Register" element={<Register />} />
+      <Route path="/Teachers" element={<Teachers />} />
+      <Route path="/Student" element={<Student />} />
+      <Route path="/Billings" element={<Billings />} />
+      <Route path="/Setting" element={<Setting />} />
+      <Route path="/Exams" element={<Exams />} />
     </Routes>
   
     </>
